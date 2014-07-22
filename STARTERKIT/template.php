a1005d6898d1e0b728f066b9444fb1655ca00d4a
Markus Oberlehner <markus.oberlehner@gmail.com>
2014-07-22 16:22:02 +0200

    changed function call of hook_form_alter to a named function call (STARTERKIT_form_search_block_form_alter), fixes #17

--- STARTERKIT/template.php
+++ STARTERKIT/template.php
@@ -1,10 +1,8 @@
 <?php
 
-function STARTERKIT_form_alter(&$form, &$form_state, $form_id) {
-  if ($form_id == 'search_block_form') {
-    // HTML5 placeholder attribute
-    $form['search_block_form']['#attributes']['placeholder'] = t('Ihr Suchbegriff') . '...';
-  }
+function STARTERKIT_form_search_block_form_alter(&$form, &$form_state, $form_id) {
+  // HTML5 placeholder attribute
+  $form['search_block_form']['#attributes']['placeholder'] = t('Ihr Suchbegriff') . '...';
 }
 
 /**
