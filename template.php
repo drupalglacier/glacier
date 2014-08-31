167c628aacc06cd37fc39cde6aebd52a5fa4ad94
Markus Oberlehner <markus.oberlehner@gmail.com>
2014-08-31 09:23:31 +0200

    added message scss component, according to #5

--- template.php
+++ template.php
@@ -38,6 +38,35 @@ function glacier_preprocess_button(&$vars) {
   }
 }
 
+function glacier_status_messages($vars) {
+  $display = $vars['display'];
+  $output = '';
+
+  $status_heading = array(
+    'status' => t('Status message'),
+    'error' => t('Error message'),
+    'warning' => t('Warning message'),
+  );
+  foreach (drupal_get_messages($display) as $type => $messages) {
+    $output .= "<div class=\"message message--$type\">\n";
+    if (!empty($status_heading[$type])) {
+      $output .= '<h2 class="element-invisible">' . $status_heading[$type] . "</h2>\n";
+    }
+    if (count($messages) > 1) {
+      $output .= " <ul>\n";
+      foreach ($messages as $message) {
+        $output .= '  <li>' . $message . "</li>\n";
+      }
+      $output .= " </ul>\n";
+    }
+    else {
+      $output .= $messages[0];
+    }
+    $output .= "</div>\n";
+  }
+  return $output;
+}
+
 /**
  * Implements template_preprocess_html().
  */
