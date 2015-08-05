69fb22803ed202c95fd7b310e9b445716ef09ec9
Markus Oberlehner <markus.oberlehner@gmail.com>
2015-08-05 11:29:43 +0200

    removed page.tpl.php from the base theme and changed the if conditions in the STARTERKIT version of the file to check if a region is renderable

--- STARTERKIT/templates/system/page.tpl.php
+++ STARTERKIT/templates/system/page.tpl.php
@@ -4,26 +4,26 @@
  * Theme implementation to display a single Drupal page.
  */
 ?>
-<?php if ($page['header'] || render($page['navigation'])): ?>
+<?php if (render($page['header']) || render($page['navigation'])): ?>
   <header class="c-main-header" role="banner">
     <?php print render($page['header']); ?>
     <?php print render($page['navigation']); ?>
   </header>
 <?php endif; ?>
 
-<?php if ($page['content']): ?>
+<?php if (render($page['content'])): ?>
   <main id="main" class="c-main-content" role="main" tabindex="-1">
     <?php print render($page['content']); ?>
   </main>
 <?php endif; ?>
 
-<?php if ($page['postscript']): ?>
+<?php if (render($page['postscript'])): ?>
   <div class="c-postscript">
     <?php print render($page['postscript']); ?>
   </div>
 <?php endif; ?>
 
-<?php if ($page['footer']): ?>
+<?php if (render($page['footer'])): ?>
   <footer class="c-main-footer" role="contentinfo">
     <?php print render($page['footer']); ?>
   </footer>
