0c47ca37d552119d231af4b27b36d4e0d34775e3
Markus Oberlehner <markus.oberlehner@gmail.com>
2015-08-12 10:52:53 +0200

    prefixed class names in block tpl files

--- templates/block/block--navigation.tpl.php
+++ templates/block/block--navigation.tpl.php
@@ -3,14 +3,42 @@
  * @file
  * Theme implementation to display a single block.
  */
+
+$classes = glacier_classes(
+  array(
+    // 'c-block',
+    // 'c-block--' . $module_class,
+    // 'c-block--' . $delta_class,
+    'c-' . $delta_class,
+  ),
+  $classes_array
+);
+
+$title_classes = glacier_classes(
+  array(
+    // 'c-block__title',
+    'c-' . $delta_class . '__title',
+  ),
+  $title_classes_array
+);
+
+$content_classes = glacier_classes(
+  array(
+    // 'c-block__content',
+    'c-' . $delta_class . '__content',
+  ),
+  $content_classes_array
+);
+
 ?>
 <nav id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
-
   <?php print render($title_prefix); ?>
   <?php if ($block->subject): ?>
-    <h2<?php print $title_attributes; ?>><?php print $block->subject ?></h2>
+    <h2 class="<?php print $title_classes; ?>"<?php print $title_attributes; ?>><?php print $block->subject ?></h2>
   <?php endif;?>
   <?php print render($title_suffix); ?>
 
-  <?php print $content ?>
+  <?php /*<div class="<?php print $content_classes; ?>"<?php print $content_attributes; ?>>*/ ?>
+    <?php print $content ?>
+  <?php /*</div>*/ ?>
 </nav>
