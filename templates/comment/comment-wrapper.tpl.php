248889119971d1dc838803bdc8e1713178a742c9
Markus Oberlehner <markus.oberlehner@gmail.com>
2015-08-12 11:11:40 +0200

    prefixed class names in comment tpl files

--- templates/comment/comment-wrapper.tpl.php
+++ templates/comment/comment-wrapper.tpl.php
@@ -4,21 +4,44 @@
  * Theme implementation to provide an HTML container for comments.
  */
 
+$classes = glacier_classes(
+  array(
+    'c-comments',
+  ),
+  $classes_array
+);
+
+$title_classes = glacier_classes(
+  array(
+    'c-comments__title',
+  ),
+  $title_classes_array
+);
+
+$form_title_classes = glacier_classes(
+  array(
+    'c-comments__title',
+    'c-comments__title--form',
+  ),
+  $form_title_classes_array
+);
+
 // Render the comments and form first to see if we need headings.
 $comments = render($content['comments']);
 $comment_form = render($content['comment_form']);
+
 ?>
-<section<?php print $attributes; ?>>
+<section class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php if ($comments && $node->type != 'forum'): ?>
     <?php print render($title_prefix); ?>
-    <h2<?php print $title_attributes ?>><?php print t('Comments'); ?></h2>
+    <h2 class="<?php print $title_classes; ?>"<?php print $title_attributes ?>><?php print t('Comments'); ?></h2>
     <?php print render($title_suffix); ?>
   <?php endif; ?>
 
   <?php print $comments; ?>
 
   <?php if ($comment_form): ?>
-    <h2<?php print $form_title_attributes ?>><?php print t('Add new comment'); ?></h2>
+    <h2 class="<?php print $form_title_classes; ?>"<?php print $form_title_attributes ?>><?php print t('Add new comment'); ?></h2>
     <?php print $comment_form; ?>
   <?php endif; ?>
 </section>
--- templates/comment/comment.tpl.php
+++ templates/comment/comment.tpl.php
@@ -3,16 +3,37 @@
  * @file
  * Theme implementation for comments.
  */
+
+$classes = glacier_classes(
+  array(),
+  $classes_array
+);
+
+$title_classes = glacier_classes(
+  array(
+    'c-comment__title',
+  ),
+  $title_classes_array
+);
+
+$content_classes = glacier_classes(
+  array(
+    'c-comment__content',
+  ),
+  $content_classes_array
+);
+
 hide($content['links']);
+
 ?>
-<article<?php print $attributes; ?>>
+<article class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php print render($title_prefix); ?>
 
   <?php if ($new): ?>
-    <mark class="comment__new"><?php print $new; ?></mark>
+    <mark class="c-comment__new"><?php print $new; ?></mark>
   <?php endif; ?>
 
-  <h3<?php print $title_attributes; ?>>
+  <h3 class="<?php print $title_classes; ?>"<?php print $title_attributes; ?>>
     <?php print $title; ?>
   </h3>
 
@@ -20,18 +41,18 @@ hide($content['links']);
 
   <footer>
     <?php print $user_picture; ?>
-    <p class="comment__submitted">
-      <cite class="comment__author"><?php print $author; ?></cite>
+    <p class="c-comment__submitted">
+      <cite class="c-comment__author"><?php print $author; ?></cite>
       <?php print $created; ?>
     </p>
     <?php print $permalink; ?>
   </footer>
 
-  <div<?php print $content_attributes; ?>>
+  <div class="<?php print $content_classes; ?>"<?php print $content_attributes; ?>>
     <?php print render($content); ?>
 
     <?php if ($signature): ?>
-    <aside class="comment__signature">
+    <aside class="c-comment__signature">
       <?php print $signature; ?>
     </aside>
     <?php endif; ?>
