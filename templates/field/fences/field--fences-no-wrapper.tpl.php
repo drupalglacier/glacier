8fef095c47f91d46415d992f4697bcffda7e2b10
Markus Oberlehner <markus.oberlehner@gmail.com>
2015-08-22 16:02:37 +0200

    removed the item wrapper from the 'no-wrapper' fences field tpl

--- templates/field/fences/field--fences-no-wrapper.tpl.php
+++ templates/field/fences/field--fences-no-wrapper.tpl.php
@@ -8,7 +8,5 @@
   <div class="<?php print $label_classes; ?>"<?php print $title_attributes; ?>><?php print $label; ?></div>
 <?php endif; ?>
 <?php foreach ($items as $delta => $item): ?>
-  <?php if (!$item_wrapper_hidden): ?><div class="<?php print $item_classes; ?>"<?php print $item_attributes[$delta]; ?>><?php endif; ?>
-    <?php print render($item); ?>
-  <?php if (!$item_wrapper_hidden): ?></div><?php endif; ?>
+  <?php print render($item); ?>
 <?php endforeach; ?>
