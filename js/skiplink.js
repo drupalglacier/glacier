97ab671a48bcc6bfc1446e0e50cc1dcf6f48712a
Markus Oberlehner <markus.oberlehner@gmail.com>
2015-08-27 21:38:59 +0200

    changed JavaScript files to use 'var' in front of every variable declaration (instead single 'var' with ',')

--- js/skiplink.js
+++ js/skiplink.js
@@ -1,14 +1,13 @@
 jQuery(function($) {
-  var is_webkit = navigator.userAgent.toLowerCase().indexOf('webkit') > -1,
-      is_opera = navigator.userAgent.toLowerCase().indexOf('opera') > -1;
+  var is_webkit = navigator.userAgent.toLowerCase().indexOf('webkit') > -1;
+  var is_opera = navigator.userAgent.toLowerCase().indexOf('opera') > -1;
 
   if (is_webkit || is_opera) {
     $('.c-skiplink__link').each(function() {
-      var $skip_link = $(this),
-          $target = $($skip_link.attr('href'));
+      var $skip_link = $(this);
+      var $target = $($skip_link.attr('href'));
 
       $target.attr('tabindex', '0');
-
       $skip_link.attr('onclick', 'document.getElementById(\'' + $target.attr('id') + '\').focus();');
     });
   }
