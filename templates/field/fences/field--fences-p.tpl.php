4ab7123ff944c587ae196bf608402b8998966e9c
Markus Oberlehner <markus.oberlehner@gmail.com>
2015-09-15 21:03:29 +0200

    changed div tags to span tags inside of the p field tpl, fixes #80

--- templates/field/fences/field--fences-p.tpl.php
+++ templates/field/fences/field--fences-p.tpl.php
@@ -8,11 +8,11 @@
 ?>
 <p class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php if (!$label_hidden): ?>
-    <div class="<?php print $label_classes; ?>"<?php print $title_attributes; ?>><?php print $label; ?></div>
+    <span class="<?php print $label_classes; ?>"<?php print $title_attributes; ?>><?php print $label; ?></span>
   <?php endif; ?>
   <?php foreach ($items as $delta => $item): ?>
-    <?php if (!$item_wrapper_hidden): ?><div class="<?php print $item_classes; ?>"<?php print $item_attributes[$delta]; ?>><?php endif; ?>
+    <?php if (!$item_wrapper_hidden): ?><span class="<?php print $item_classes; ?>"<?php print $item_attributes[$delta]; ?>><?php endif; ?>
       <?php print render($item); ?>
-    <?php if (!$item_wrapper_hidden): ?></div><?php endif; ?>
+    <?php if (!$item_wrapper_hidden): ?></span><?php endif; ?>
   <?php endforeach; ?>
 </p>
