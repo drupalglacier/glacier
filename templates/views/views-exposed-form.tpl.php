cd2bdf47619cc34dca6d591f6b847cdd9b2b88b7
Markus Oberlehner <markus.oberlehner@gmail.com>
2015-11-24 18:47:36 +0100

    changed the rendering of views exposed form elements to be more consistent with the rendering of other form elements - the form item label is now rendered inside the form item container, according to #75

--- templates/views/views-exposed-form.tpl.php
+++ templates/views/views-exposed-form.tpl.php
@@ -14,22 +14,12 @@
 <?php if (!$classes_default): ?>
   <?php foreach ($widgets as $id => $widget): ?>
     <div id="<?php print $widget->id; ?>" class="c-view__widget c-view__widget--<?php print $id; ?>">
-      <?php if (!empty($widget->label)): ?>
-        <label class="c-view__widget__label" for="<?php print $widget->id; ?>">
-          <?php print $widget->label; ?>
-        </label>
-      <?php endif; ?>
       <?php if (!empty($widget->operator)): ?>
         <div class="c-view__widget__operator">
           <?php print $widget->operator; ?>
         </div>
       <?php endif; ?>
       <?php print $widget->widget; ?>
-      <?php if (!empty($widget->description)): ?>
-        <div class="c-view__widget__description">
-          <?php print $widget->description; ?>
-        </div>
-      <?php endif; ?>
     </div>
   <?php endforeach; ?>
   <?php if (!empty($sort_by)): ?>
@@ -62,22 +52,12 @@
   <div class="c-view__exposed-form">
     <?php foreach ($widgets as $id => $widget): ?>
       <div id="<?php print $widget->id; ?>-wrapper" class="c-view__widget view__widget--<?php print $id; ?>">
-        <?php if (!empty($widget->label)): ?>
-          <label class="c-view__widget__label" for="<?php print $widget->id; ?>">
-            <?php print $widget->label; ?>
-          </label>
-        <?php endif; ?>
         <?php if (!empty($widget->operator)): ?>
           <div class="c-view__widget__operator">
             <?php print $widget->operator; ?>
           </div>
         <?php endif; ?>
         <?php print $widget->widget; ?>
-        <?php if (!empty($widget->description)): ?>
-          <div class="c-view__widget__description">
-            <?php print $widget->description; ?>
-          </div>
-        <?php endif; ?>
       </div>
     <?php endforeach; ?>
     <?php if (!empty($sort_by)): ?>
